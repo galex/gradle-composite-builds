13d6a379f28758862cfc3b34870bb72acb2f3f60
Alexandre Gherschon <alexandre.gherschon@gmail.com>
2017-08-02 08:43:44 +0300

    Functionning project that demonstrates the use of Gradle Composite builds

---
+++ comp-client/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+/**
+ * Created by galex on 19/07/2017.
+ */
+
+fun main(args: Array<String>) {
+    val gradle = "Gradle"
+    println(gradle.addAwesomeness())
+    println(gradle.addGrandiosity())
+}
---
+++ comp-sdk/src/main/kotlin/StringExtensions.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by galex on 19/07/2017.
+ */
+
+fun String.addAwesomeness() = "$this is awesome!"
+
+fun String.addGrandiosity() = "$this is grandiose!"
